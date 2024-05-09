bed41a2cd1389a27be2956b96e1af09147c7b4ed
kenyantequila <abdishakur431@gmail.com>
2024-05-09 12:35:54 +0300

    commmit

--- src/Components/Notification.js
+++ src/Components/Notification.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Bounce, Flip, ToastContainer, toast } from'react-toastify';
 import'react-toastify/dist/ReactToastify.css';
+import './Notification.css'
 
 function Notification() {
   const notify = () => 
@@ -8,13 +9,18 @@ function Notification() {
   
   const show = () => 
     toast.info("Here are list of Avators!");
+  toast.info("select prefered Avator while scrolling along them!");
+ 
   const provide = () => 
     toast.warning("are you sure!");
+  
   return (
     <div>
+      <div className='btn'>
           <button onClick={notify}>select!</button>
           <button onClick={show}>choose!</button>
           <button onClick={provide}>choose!</button>
+          </div>
           
         <ToastContainer 
         position='top-center'
